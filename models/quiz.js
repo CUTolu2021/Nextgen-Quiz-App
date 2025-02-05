991fcd04dd5c8348520007c6f72c3c8a19a75327
Omoniyi Tolulope <omoniyitolulope05@gmail.com>
2025-02-05 15:20:00 +0100

    merge complete

--- models/quiz.js
+++ models/quiz.js
@@ -43,77 +43,23 @@ const quizSchema = new Schema({
 }, {
     timestamps: true,
 });
+ 
 
-module.exports = model('Quiz', quizSchema);const { Schema, model } = require('mongoose');
-const UserResponse = require("../models/UserResponse");
-const quizScores = new Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    description: {
-        type: String,
-        required: true,
-    },
-    questions: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Question',
-        },
-    ],
-    creator_id: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true,
-    },
-    settings: {
-        type: Object,
-        required: true,
-    },
-    active_status: {
-        type: String,
-        enum: ['active', 'inactive'],
-        default: 'active',
-    },
-    status: {
-        type: String,
-        enum: ['Published', 'Draft'],
-        default: 'Draft',
-    },
-}, {
-    timestamps: true,
-});
-// Fetching all quizzes
+// //Creating quiz attempt, this model tracks each user's quiz attempts and stores the final score.
+// const quizAttemptSchema = new mongoose.Schema({
+//     userId: { type: String, required: true },
+//     quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz", required: true },
+//     score: { type: Number, required: true },
+//     totalQuestions: { type: Number, required: true },
+//     completedAt: { type: Date, default: Date.now }
+// });
 
-const quiz = new mongoose.Schema({
-  questions: { type: String, required: true },
-  title: [{ type: String, required: true }],
-  description: { type: String, required: true },
-});
-
-
-
-
-//Creating quiz attempt, this model tracks each user's quiz attempts and stores the final score.
-const quizAttemptSchema = new mongoose.Schema({
-    userId: { type: String, required: true },
-    quizId: { type: mongoose.Schema.Types.ObjectId, ref: "Quiz", required: true },
-    score: { type: Number, required: true },
-    totalQuestions: { type: Number, required: true },
-    completedAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model(quizAttemptSchema, quiz, quizScores);
+// module.exports = mongoose.model(quizAttemptSchema, quiz, quizScores);
 
 
 
 //Defining the schema for quiz attempt
 const quizAttemptSchema = new Schema({
-    // attemptId: {
-    //     type: String,
-    //     required: true,
-    //     unique: true
-    // },
     userId: {
         type: Schema.Types.ObjectId,
         ref: 'User',
